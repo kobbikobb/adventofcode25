605749561181ec09743c92045d7b2ca8852e8109
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-12-04 17:15:51 +0000

    fix

--- day01/solution.py
+++ day01/solution.py
@@ -41,18 +41,26 @@ def get_hits_part_2(data: str):
         number = int(line[1:])
 
         if digit == "L":
+            org = start
             start = start - number
             if start < 0:
-                start = (100 + start) % 100
+                hit = hit + abs(start) // 100
+                if org != 0:
+                    hit = hit + 1
+
+                start = 100 + start
+            elif start == 0:
+                hit = hit + 1
         elif digit == "R":
             start = start + number
             if start > 99:
-                start = (start - 100) % 100
+                hit = hit + abs(start) // 100
+
+                start = start - 100
         else:
             raise Exception("Illegal input")
 
-        if start == 0:
-            hit = hit + 1
+        start = start % 100
 
     return hit
 
