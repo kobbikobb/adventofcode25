fe0437dc4268aca72c921ba15e742609f7af27a1
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-12-06 23:10:30 +0000

    day 6 complete

--- day06/part2.py
+++ day06/part2.py
@@ -1,3 +1,5 @@
+import re
+
 from _utils.text_utils import get_lines
 
 
@@ -13,24 +15,58 @@ def calculate(symbol: str, numbers: list[int]) -> int:
         raise ValueError(f"Unknown symbol: {symbol}")
 
 
+def get_cephalopod_numbers(column: list[str]) -> list[int]:
+    numbers = []
+
+    longest = max(len(s) for s in column)
+
+    for i in range(longest):
+
+        new: str = ""
+        for number in column:
+            if len(number) > i:
+                digit: str = number[i]
+                if digit != " ":
+                    new += digit
+
+        if len(new) > 0:
+            numbers.append(int(new))
+
+    return numbers
+
+
 def get_result_part_2(data: str):
     """Gets the result"""
 
     lines = get_lines(data)
-    symbols = lines[len(lines) - 1].split()
+    last_line = lines[len(lines) - 1]
 
-    columns: list[list[int]] = []
+    symbols: list[tuple[str, int]] = []
+
+    for i, c in enumerate(last_line):
+        if c != " ":
+            symbols.append((c, i))
+
+    columns: list[list[str]] = []
 
     for i in range(0, len(lines) - 1):
-        numbers = list(map(int, lines[i].split()))
-        for j, number in enumerate(numbers):
+        line = lines[i]
+
+        for j in range(len(symbols)):
             if len(columns) <= j:
                 columns.append([])
-            columns[j].append(number)
+
+            s1 = symbols[j][1]
+
+            if(j >= len(symbols) - 1):
+                columns[j].append(line[s1:])
+            else:
+                s2 = symbols[j + 1][1]
+                columns[j].append(line[s1: s2-1])
 
     result = 0
-    for i, symbol in enumerate(symbols):
-        column = columns[i]
-        result += calculate(symbol, column)
+    for i, column in enumerate(columns):
+        new_numbers = get_cephalopod_numbers(column)
+        result += calculate(symbols[i][0], new_numbers)
 
     return result
--- day06/test_part_2.py
+++ day06/test_part_2.py
@@ -5,6 +5,8 @@ from day06.part2 import get_result_part_2
 
 class TestDay06Part2(unittest.TestCase):
 
+
+
     def test1(self):
         test_data = """123 328  51 64 
  45 64  387 23 
