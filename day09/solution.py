0069001b18047452c9a7d053e844055f0289428f
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-12-11 23:04:36 +0000

    Day 9b, slow

--- day09/solution.py
+++ day09/solution.py
@@ -2,15 +2,17 @@ from _utils.text_utils import read_input_data
 from day09.part1 import get_result_part_1
 from day09.part2 import get_result_part_2
 
+
 def main():
     """The solution"""
     data = read_input_data("input.txt")
 
-   # result1 = get_result_part_1(data)
-   # print(f"result 1 :{result1}")
+    result1 = get_result_part_1(data)
+    print(f"result 1 :{result1}")
 
-    result2 = (get_result_part_2(data))
+    result2 = get_result_part_2(data)
     print(f"result 2 :{result2}")
+
 
 if __name__ == "__main__":
     # 1: 4752484112 Correct
